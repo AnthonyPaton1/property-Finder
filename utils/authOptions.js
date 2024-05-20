0a9785c0ae524a5f7aa1f569fd625b48a81eba6b
anthonypaton <anthonypaton1@sky.com>
2024-05-20 20:47:08 +0100

    latest

--- utils/authOptions.js
+++ utils/authOptions.js
@@ -1,3 +1,5 @@
+import connectDB from "@/config/database";
+import User from "@/models/User";
 import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions = {
@@ -15,18 +17,34 @@ export const authOptions = {
     }),
   ],
   callbacks: {
-    //Invoked on successful sign in
+    // Invoked on successful signin
     async signIn({ profile }) {
-      //1.connect to db
-      //2.check if user exists
-      //3.if not, add user to database
-      //4. return true to allow sign in
+      // 1. Connect to database
+      await connectDB();
+      // 2. Check if user exists
+      const userExists = await User.findOne({ email: profile.email });
+      // 3. If not, then add user to database
+      if (!userExists) {
+        // Truncate user name if too long
+        const username = profile.name.slice(0, 20);
+
+        await User.create({
+          email: profile.email,
+          username,
+          image: profile.picture,
+        });
+      }
+      // 4. Return true to allow sign in
+      return true;
     },
-    //Modifies session object
+    // Modifies the session object
     async session({ session }) {
-      //1 get user from db
-      //2assign user id to session
-      //3 return session
+      // 1. Get user from database
+      const user = await User.findOne({ email: session.user.email });
+      // 2. Assign the user id to the session
+      session.user.id = user._id.toString();
+      // 3. return session
+      return session;
     },
   },
 };
